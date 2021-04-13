89b80074f3135d85c3685e0e881a9a61de4144f9
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-04-13 12:57:29 -1000

    edit: adds student bios

--- src/_data/students.js
+++ src/_data/students.js
@@ -2,42 +2,52 @@ module.exports = [
   // TODO: Add student videos
   {
     name: "Ariana Enomoto",
+    bio: "hi",
     website: "/ariana",
   },
   {
     name: "Emily Freeman",
+    bio: "yo",
     website: "https://eafreeman.com",
   },
   {
     name: "Christi Gonzales",
+    bio: "this",
     website: "https://christigonzales.com",
   },
   {
     name: "Eryn Iwanaga",
+    bio: "is",
     website: "https://eiwanaga.com/portfolio",
   },
   {
     name: "Christy Liang",
+    bio: "is",
     website: "https://christyzliang.com",
   },
   {
     name: "Koreena Nagai",
+    bio: "is",
     website: "https://knagai.com",
   },
   {
     name: "Miyuki Sekiguchi",
+    bio: "is",
     website: "https://miyukings.github.io/myportfolio-v3_2020/",
   },
   {
     name: "Tomy Takemura",
+    bio: "is",
     website: "https://tomytak.com",
   },
   {
     name: "Joanna Yee",
+    bio: "is",
     website: "https://joannamedia.com",
   },
   {
     name: "David",
+    bio: "is",
     website: "https://google.com",
   },
 ];
