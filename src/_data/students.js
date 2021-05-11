a7a2348ccb122a328848dfa072ecaf8140af08b0
lummelis <lummelis@hawaii.edu>
2021-05-11 09:41:57 -1000

    added img

--- src/_data/students.js
+++ src/_data/students.js
@@ -1,100 +1,103 @@
 module.exports = [
-  // TODO: Add student videos
+    // TODO: Add student videos
 
-  {
-    name: "Reyna Jansen",
-    bio: "",
-    photo: "placeholder-student-avatar.jpg",
-    website: "https://moonmakesart.wixsite.com/gallery",
-  },
-  {
-    name: "Racyn Noguchi",
-    bio: "",
-    photo: "placeholder-student-avatar.jpg",
-    website: "https://sites.google.com/view/webdemoreeldemosite/home",
-  },
-  {
-    name: "DyLin Khoe",
-    bio: "",
-    photo: "placeholder-student-avatar.jpg",
-    website: "https://st3gmolak.wixsite.com/steg",
-  },
-  {
-    name: "Aden Kemp",
-    bio: "",
-    photo: "placeholder-student-avatar.jpg",
-    website: "https://sites.google.com/hawaii.edu/aok/home",
-  },
-  {
-    name: "Bridgette Laranio",
-    bio: "",
-    photo: "placeholder-student-avatar.jpg",
-    website: "https://sites.google.com/view/blaranio/home",
-  },
-  {
-    name: "Adrianremoh Mangahis",
-    bio: "",
-    photo: "placeholder-student-avatar.jpg",
-    website: "https://sites.google.com/view/adrianremohmangahis/home",
-  },
-  {
-    name: "Luis Pascua",
-    bio: "",
-    photo: "placeholder-student-avatar.jpg",
-    website: "https://sites.google.com/view/luispascua/home",
-  },
-  {
-    name: "Jake Camara",
-    bio: "",
-    photo: "placeholder-student-avatar.jpg",
-    website: "http://jscamara.com/",
-  },
-  {
-    name: "Crystal Kim",
-    bio: "",
-    photo: "placeholder-student-avatar.jpg",
-    website: "https://sites.google.com/hawaii.edu/rystql/art-gallery/3d",
-  },
-  {
-    name: "Venice Bagolor",
-    bio: "I am interested in learning more about design, such as logo-design, typography, branding, and more. My strength is mostly hands on artwork such as sketching and drawing but willing to strengthen my skills in transitioning to the digital world.I do enjoy learning new skill and improving myself through different task and projects.",
-    photo: "placeholder-student-avatar.jpg",
-    website: "https://mbagolor.github.io/portfolio_v2/",
-  },
-  {
-    name: "Mony Brousseau",
-    bio: "I am a beggining graphic designer, studying at KCC in the New Media Arts program. I love working on branding, logo design, advertising, and front-end web developement. While I am still a budding designer, I am passionate about my work and enjoy gaining experience and skills in my field.",
-    photo: "placeholder-student-avatar.jpg",
-    website: "https://mnb62.github.io/mochidesign/",
-  },
-  {
-    name: "Anthony Diep",
-    bio: "Born in Vietnam and currently living in Hawaii. I have an associate degree in New Media Arts, and a bachelor degree in Marketing and MIS. I found my call for design at a later stage of my career than most, and it has been an unbelievable journey. I want to invite you on that journey with me.",
-    photo: "placeholder-student-avatar.jpg",
-    website: "https://anthony-diep.github.io/portfolio/",
-  },
-  {
-    name: "Yiwen Jiang",
-    bio: "I was born in Shanghai, China. Since I was young, I have been interested in animation, games drawing and painting, so I am better at drawing class design. In addition to interface design skills, I’m also good at drawing and watercolor. I’m still improving my interface design and communication skills. ",
-    photo: "placeholder-student-avatar.jpg",
-    website: "https://yiwen-jiang.github.io/Portfolio/",
-  },
-  {
-    name: "Ai Tomoi",
-    bio: "Funny, Energetic, Yoga, Meditation, Color, Nature, Playful, Hawaii, Morrocco, India, New Mexico, Sedona, Mexico, Caligraphy, Round, Hologram, Prana. I am learning to create logo designs, typography, branding and websites through my coursework in New Media Art at Kapi‘olani Community College. ",
-    photo: "placeholder-student-avatar.jpg",
-    website: "https://aitomoi.github.io/portfolio_v1a/",
-  },
-  {
-    name: "Liana Young",
-    bio: "Graphic Designer, type-enthusiast, occasional illustrator, and copy-writer. I'm a grid-abider and grid-breaker. Oh, the duality.",
-    photo: "placeholder-student-avatar.jpg",
-    website: "https://l7ana.github.io/portfolio/index.html",
-  },
+    {
+        name: "Reyna Jansen",
+        bio: "",
+        photo: "ReynaJansen.jpg",
+        website: "https://moonmakesart.wixsite.com/gallery",
+        demoreel: "",
+    },
+    {
+        name: "Racyn Noguchi",
+        bio: "",
+        photo: "RacynNoguchi.jpg",
+        website: "https://sites.google.com/view/webdemoreeldemosite/home",
+        demoreel: "",
+    },
+    {
+        name: "DyLin Khoe",
+        bio: "",
+        photo: "DylinKhoe.jpg",
+        website: "https://st3gmolak.wixsite.com/steg",
+        demoreel: "",
+    },
+    {
+        name: "Aden Kemp",
+        bio: "",
+        photo: "AdenKemp.jpg",
+        website: "https://sites.google.com/hawaii.edu/aok/home",
+        demoreel: "",
+    },
+    {
+        name: "Bridgette Laranio",
+        bio: "",
+        photo: "BridgetteLaranio.jpg",
+        website: "https://sites.google.com/view/blaranio/home",
+        demoreel: "",
+    },
+    {
+        name: "Adrianremoh Mangahis",
+        bio: "",
+        photo: "AdrianremohMangahis.jpg",
+        website: "https://sites.google.com/view/adrianremohmangahis/home",
+        demoreel: "",
+    },
+    {
+        name: "Luis Pascua",
+        bio: "",
+        photo: "JoseLuisPascua.jpg",
+        website: "https://sites.google.com/view/luispascua/home",
+        demoreel: "",
+    },
+    {
+        name: "Jake Camara",
+        bio: "",
+        photo: "JakeCamara.jpg",
+        website: "http://jscamara.com/",
+        demoreel: "",
+    },
+    {
+        name: "Crystal Kim",
+        bio: "",
+        photo: "HyeLimKim.jpg",
+        website: "https://sites.google.com/hawaii.edu/rystql/art-gallery/3d",
+        demoreel: "",
+    },
+    {
+        name: "Venice Bagolor",
+        bio: "I am interested in learning more about design, such as logo-design, typography, branding, and more. My strength is mostly hands on artwork such as sketching and drawing but willing to strengthen my skills in transitioning to the digital world.I do enjoy learning new skill and improving myself through different task and projects.",
+        photo: "MaVeniceBagolor.jpg",
+        website: "https://mbagolor.github.io/portfolio_v2/",
+    },
+    {
+        name: "Mony Brousseau",
+        bio: "I am a beggining graphic designer, studying at KCC in the New Media Arts program. I love working on branding, logo design, advertising, and front-end web developement. While I am still a budding designer, I am passionate about my work and enjoy gaining experience and skills in my field.",
+        photo: "MonyBrousseau.jpg",
+        website: "https://mnb62.github.io/mochidesign/",
+    },
+    {
+        name: "Anthony Diep",
+        bio: "Born in Vietnam and currently living in Hawaii. I have an associate degree in New Media Arts, and a bachelor degree in Marketing and MIS. I found my call for design at a later stage of my career than most, and it has been an unbelievable journey. I want to invite you on that journey with me.",
+        photo: "AnthonyDiep.jpg",
+        website: "https://anthony-diep.com/",
+    },
+    {
+        name: "Yiwen Jiang",
+        bio: "I was born in Shanghai, China. Since I was young, I have been interested in animation, games drawing and painting, so I am better at drawing class design. In addition to interface design skills, I’m also good at drawing and watercolor. I’m still improving my interface design and communication skills. ",
+        photo: "YiwenJiang.jpg",
+        website: "https://yiwen-jiang.github.io/Portfolio/",
+    },
+    {
+        name: "Ai Tomoi",
+        bio: "Funny, Energetic, Yoga, Meditation, Color, Nature, Playful, Hawaii, Morrocco, India, New Mexico, Sedona, Mexico, Caligraphy, Round, Hologram, Prana. I am learning to create logo designs, typography, branding and websites through my coursework in New Media Art at Kapi‘olani Community College. ",
+        photo: "AiTomoi.jpg",
+        website: "https://aitomoi.github.io/portfolio_v1a/",
+    },
+    {
+        name: "Liana Young",
+        bio: "Graphic Designer, type-enthusiast, occasional illustrator, and copy-writer. I'm a grid-abider and grid-breaker. Oh, the duality.",
+        photo: "LianaYoung.jpg",
+        website: "https://lianayoung.design",
+    },
 ];
-
-
-
-
-
-
