64720a323f806cd7eb61b0a77db5e9cdd45387a7
lummelis <lummelis@hawaii.edu>
2021-05-13 09:54:49 -1000

    added specialization remove opacity

--- src/_data/students.js
+++ src/_data/students.js
@@ -7,6 +7,7 @@ module.exports = [
         photo: "ReynaJansen.jpg",
         website: "https://moonmakesart.wixsite.com/gallery",
         demoreel: "",
+        specialization: "Animation",
     },
     {
         name: "Racyn Noguchi",
@@ -14,6 +15,7 @@ module.exports = [
         photo: "racyn-noguchi.jpg",
         website: "https://sites.google.com/view/webdemoreeldemosite/home",
         demoreel: "",
+        specialization: "Animation",
     },
     {
         name: "DyLin Khoe",
@@ -21,6 +23,7 @@ module.exports = [
         photo: "DylinKhoe.jpg",
         website: "https://st3gmolak.wixsite.com/steg",
         demoreel: "",
+        specialization: "Animation",
     },
     {
         name: "Aden Kemp",
@@ -28,6 +31,7 @@ module.exports = [
         photo: "AdenKemp.jpg",
         website: "https://sites.google.com/hawaii.edu/aok/home",
         demoreel: "",
+        specialization: "Animation",
     },
     {
         name: "Bridgette Laranio",
@@ -35,6 +39,7 @@ module.exports = [
         photo: "BridgetteLaranio.jpg",
         website: "https://sites.google.com/view/blaranio/home",
         demoreel: "",
+        specialization: "Animation",
     },
     {
         name: "Adrianremoh Mangahis",
@@ -42,6 +47,7 @@ module.exports = [
         photo: "AdrianremohMangahis.jpg",
         website: "https://sites.google.com/view/adrianremohmangahis/home",
         demoreel: "",
+        specialization: "Animation",
     },
     {
         name: "Luis Pascua",
@@ -49,6 +55,7 @@ module.exports = [
         photo: "JoseLuisPascua.jpg",
         website: "https://sites.google.com/view/luispascua/home",
         demoreel: "",
+        specialization: "Animation",
     },
     {
         name: "Jake Camara",
@@ -56,6 +63,7 @@ module.exports = [
         photo: "JakeCamara.jpg",
         website: "http://jscamara.com/",
         demoreel: "",
+        specialization: "Animation",
     },
     {
         name: "Crystal Kim",
@@ -63,41 +71,48 @@ module.exports = [
         photo: "HyeLimKim.jpg",
         website: "https://sites.google.com/hawaii.edu/rystql/art-gallery/3d",
         demoreel: "",
+        specialization: "Animation",
     },
     {
         name: "Venice Bagolor",
         bio: "I am interested in learning more about design, such as logo-design, typography, branding, and more. My strength is mostly hands on artwork such as sketching and drawing but willing to strengthen my skills in transitioning to the digital world.I do enjoy learning new skill and improving myself through different task and projects.",
         photo: "MaVeniceBagolor.jpg",
         website: "https://mbagolor.github.io/portfolio_v2/",
+        specialization: "Interface Design",
     },
     {
         name: "Mony Brousseau",
         bio: "I am a beggining graphic designer, studying at KCC in the New Media Arts program. I love working on branding, logo design, advertising, and front-end web developement. While I am still a budding designer, I am passionate about my work and enjoy gaining experience and skills in my field.",
         photo: "MonyBrousseau.jpg",
         website: "https://mnb62.github.io/mochidesign/",
+        specialization: "Interface Design",
     },
     {
         name: "Anthony Diep",
         bio: "Born in Vietnam and currently living in Hawaii. I have an associate degree in New Media Arts, and a bachelor degree in Marketing and MIS. I found my call for design at a later stage of my career than most, and it has been an unbelievable journey. I want to invite you on that journey with me.",
         photo: "AnthonyDiep.jpg",
         website: "https://anthony-diep.com/",
+        specialization: "Interface Design",
     },
     {
         name: "Yiwen Jiang",
         bio: "I was born in Shanghai, China. Since I was young, I have been interested in animation, games drawing and painting, so I am better at drawing class design. In addition to interface design skills, I’m also good at drawing and watercolor. I’m still improving my interface design and communication skills. ",
         photo: "YiwenJiang.jpg",
         website: "https://yiwen-jiang.github.io/Portfolio/",
+        specialization: "Interface Design",
     },
     {
         name: "Ai Tomoi",
         bio: "Funny, Energetic, Yoga, Meditation, Color, Nature, Playful, Hawaii, Morrocco, India, New Mexico, Sedona, Mexico, Caligraphy, Round, Hologram, Prana. I am learning to create logo designs, typography, branding and websites through my coursework in New Media Art at Kapi‘olani Community College. ",
         photo: "AiTomoi.jpg",
         website: "https://aitomoi.github.io/portfolio_v1a/",
+        specialization: "Interface Design",
     },
     {
         name: "Liana Young",
         bio: "Graphic Designer, type-enthusiast, occasional illustrator, and copy-writer. I'm a grid-abider and grid-breaker. Oh, the duality.",
         photo: "LianaYoung.jpg",
         website: "https://lianayoung.design",
+        specialization: "Interface Design",
     },
 ];
